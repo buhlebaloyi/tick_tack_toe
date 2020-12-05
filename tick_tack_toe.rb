d4177d835f3aaf4ca4474a77712a907d729dfcba
buhlebaloyi <70333790+buhlebaloyi@users.noreply.github.com>
2020-12-05 21:45:12 +0200

    Update tick_tack_toe.rb

--- tick_tack_toe.rb
+++ tick_tack_toe.rb
@@ -1,94 +1,96 @@
 class TickTackToe
-    #include PlayerMove
     attr_reader :player_pick
-    attr_reader :player_no
-    def initialize(player_no, player_pick)
-      @player_no = player_no
+    def initialize(player_pick = gets.chomp)
+      @player_no = true #true for player 1
       @player_pick = player_pick
-      $old_game = {}
+      @old_game = []
+      @winner = []
     
     end
 
 end
 
 class PlayerChoices < TickTackToe
-    def player_coice(player_no)
-        if player_no == 1
+    def player_coice()
+        if @player_no == true
             player_symbol = "X"
             return player_symbol
             p old_game
 
-        elsif player_no == 2
+        elsif @player_no == false
             player_symbol = "O"
             return player_symbol
             p old_game
-
-        else
-            puts "Please enter valid plater number. Options are 1 or 2"
 
         end
     
     end
 
-    private
-
 end
 
 class Screen < PlayerChoices
     def disp_screen()
-        p "| #{   top_left} |" + " #{   top_center} |" + " #{   top_right} |"
-        p "| #{middle_left} |" + " #{middle_center} |" + " #{middle_right} |"
-        p "| #{bottom_left} |" + " #{bottom_center} |" + " #{bottom_right} |"
+        puts "_____________"
+        puts "| #{   top_left} |" + " #{   top_center} |" + " #{   top_right} |"
+        puts "_____________"
+        puts "| #{middle_left} |" + " #{middle_center} |" + " #{middle_right} |"
+        puts "_____________"
+        puts "| #{bottom_left} |" + " #{bottom_center} |" + " #{bottom_right} |"
+        puts "_____________"
     
     end
     
 end
 
 class PlayerMove < PlayerChoices
-    def player_selection(player_pick)
-        old_game = []
-        if player_pick == 1
-            top_left = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
-            
-        elsif player_pick == 2
-            top_center = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+    def player_selection()
+        @old_game = []
+        available_moves()
+        if @avail_move.include?(@player_pick)
+            if @player_pick == 1
+                top_left = player_coice(@player_no)
+                @old_game[@player_pick-1] = player_coice()
+                
+            elsif @player_pick == 2
+                top_center = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 3
-            top_right = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 3
+                top_right = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 4
-            middle_left = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 4
+                middle_left = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 5
-            middle_center = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 5
+                middle_center = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 6
-            middle_right = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 6
+                middle_right = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 7
-            bottom_left = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 7
+                bottom_left = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 8
-            bottom_center = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 8
+                bottom_center = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        elsif player_pick == 9
-            bottom_right = player_coice(player_no)
-            old_game[player_pick-1] = player_symbol
+            elsif @player_pick == 9
+                bottom_right = player_coice(@player_no)
+                @old_game[@player_pick-1] =  player_coice()
 
-        else
-            puts "Player input move not recognised, please play a regognised move. Recodnised moves being 0 to 10"
+            else
+                puts "Player input move not recognised, please play a regognised move. Recodnised moves being 0 to 10"
+
+            end
 
         end
 
-        return old_game
+        return @old_game
 
     end
 
@@ -96,62 +98,113 @@ end
 
 class GamePlay < Screen
     def self.playgame()
-        until (old_game.length > 9) or (match_three() == true) do
-            puts "Enter player number: "
-            player_no = gets.chomp
-            puts "Enter position to play: "
-            player_coice = gets.chomp
-            #Starting new instance of game play
-            play_game = TickTackToe.new(player_no, player_coice)
-            disp_screen()
+        if @old_game.nil? != true
+            until (@old_game.length > 9) | (match_three() == true) do
+                disp_screen()
+
+            end
+
+        else
 
         end
 
     end
 
     def match_three()
-        if (old_game[0] == old_game[3]) && (old_game[0] == old_game[6])
+        if (@old_game[0] == @old_game[3]) && (@old_game[0] == @old_game[6])
+            @winner = @old_game[0]
             return true
 
-        elsif (old_game[1] == old_game[4]) && (old_game[1] == old_game[7])
+        elsif (@old_game[1] == @old_game[4]) && (@old_game[1] == @old_game[7])
+            @winner[0] = @old_game[1]
             return true
 
-        elsif (old_game[2] == old_game[5]) && (old_game[2] == old_game[8])
+        elsif (@old_game[2] == @old_game[5]) && (@old_game[2] == @old_game[8])
+            @winner[0] = @old_game[2]
             return true
 
-        elsif (old_game[0] == old_game[1]) && (old_game[0] == old_game[2])
+        elsif (@old_game[0] == @old_game[1]) && (@old_game[0] == @old_game[2])
+            @winner[0] = @old_game[0]
             return true
 
-        elsif (old_game[3] == old_game[4]) && (old_game[3] == old_game[5])
+        elsif (@old_game[3] == @old_game[4]) && (@old_game[3] == @old_game[5])
+            @winner[0] = @old_game[3]
             return true
 
-        elsif (old_game[6] == old_game[7]) && (old_game[6] == old_game[8])
+        elsif (@old_game[6] == @old_game[7]) && (@old_game[6] == @old_game[8])
+            @winner[0] = @old_game[6]
             return true
 
-        elsif (old_game[0] == old_game[4]) && (old_game[0] == old_game[8])
+        elsif (@old_game[0] == @old_game[4]) && (old_game[0] == @old_game[8])
+            @winner = @old_game[0]
             return true
 
-        elsif (old_game[2] == old_game[4]) && (old_game[2] == old_game[6])
+        elsif (@old_game[2] == @old_game[4]) && (@old_game[2] == @old_game[6])
+            @winner = @old_game[2]
             return true
+
+        else
+            return false
 
         end
 
     end
 
-    def self.pausegame()
-        return old_game
+    def pausegame()
+        puts @old_game
+        puts "Game paused!"
+        return @game_on = false, @old_game
 
     end
 
-    def self.terminategame()
-        if (player_coice == stop) or 
-            puts "Game Over player #{} won the game"
+    def playgame()
+        while @game_on == true
+            if (@player_pick.upcase == "S") | (@player_pick.upcase == "STOP") 
+                if @player_no == true
+                    puts "Game Over! Player #{2} won the game"
+                    break
+
+                elsif @player_no == false
+                    puts "Game Over! Player #{2} won the game"
+                    break 
+
+                else
+                    puts "Game over! Player quiting not specified."
+
+                end
+
+            elsif @old_game.length == 9
+                puts "Game Over. Draw"
+                break #if @old_game.length == 9
+
+            elsif match_three() == true
+                p @winner
+
+            elsif (@player_pick.upcase == "P") | (@player_pick.upcase == "PAUSE")
+                pausegame()
+
+            end
+
+            switch_player()
 
         end
 
+    end
+
+    def switch_player()
+        @player_no = !@player_no
+
+    end
+
+    def available_moves()
+        @old_game.select.with_index { |avail_move, idx| idx.nil? }
+        return avail_move
+        
     end
 
 end
 
 #super is useful for instance variables
 #new_game = TickTackToe.new(1,1)
+
+play_game_ = GamePlay.playgame
