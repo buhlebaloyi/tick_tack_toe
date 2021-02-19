715a4b2e9300841a3eeff5adf232945becca1a4e
buhlebaloyi <70333790+buhlebaloyi@users.noreply.github.com>
2021-02-19 23:02:51 +0200

    Update tick_tack_toe.rb
    A working game

--- tick_tack_toe.rb
+++ tick_tack_toe.rb
@@ -1,215 +1,3 @@
-class TickTackToe
-    attr_reader :player_pick
-    def initialize(player_pick = gets.chomp)
-      @player_no = true #true for player 1
-      @player_pick = player_pick
-      @old_game = []
-      @winner = []
-    
-    end
-
-end
-
-class PlayerChoices < TickTackToe
-    def player_coice()
-        if @player_no == true
-            player_symbol = "X"
-            return player_symbol
-            p old_game
-
-        elsif @player_no == false
-            player_symbol = "O"
-            return player_symbol
-            p old_game
-
-        end
-    
-    end
-
-end
-
-class Screen < PlayerChoices
-    def disp_screen()
-        puts "_____________"
-        puts "| #{   top_left} |" + " #{   top_center} |" + " #{   top_right} |"
-        puts "_____________"
-        puts "| #{middle_left} |" + " #{middle_center} |" + " #{middle_right} |"
-        puts "_____________"
-        puts "| #{bottom_left} |" + " #{bottom_center} |" + " #{bottom_right} |"
-        puts "_____________"
-    
-    end
-    
-end
-
-class PlayerMove < PlayerChoices
-    def player_selection()
-        @old_game = []
-        available_moves()
-        if @avail_move.include?(@player_pick)
-            if @player_pick == 1
-                top_left = player_coice(@player_no)
-                @old_game[@player_pick-1] = player_coice()
-                
-            elsif @player_pick == 2
-                top_center = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 3
-                top_right = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 4
-                middle_left = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 5
-                middle_center = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 6
-                middle_right = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 7
-                bottom_left = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 8
-                bottom_center = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            elsif @player_pick == 9
-                bottom_right = player_coice(@player_no)
-                @old_game[@player_pick-1] =  player_coice()
-
-            else
-                puts "Player input move not recognised, please play a regognised move. Recodnised moves being 0 to 10"
-
-            end
-
-        end
-
-        return @old_game
-
-    end
-
-end
-
-class GamePlay < Screen
-    def self.playgame()
-        if @old_game.nil? != true
-            until (@old_game.length > 9) | (match_three() == true) do
-                disp_screen()
-
-            end
-
-        else
-
-        end
-
-    end
-
-    def match_three()
-        if (@old_game[0] == @old_game[3]) && (@old_game[0] == @old_game[6])
-            @winner = @old_game[0]
-            return true
-
-        elsif (@old_game[1] == @old_game[4]) && (@old_game[1] == @old_game[7])
-            @winner[0] = @old_game[1]
-            return true
-
-        elsif (@old_game[2] == @old_game[5]) && (@old_game[2] == @old_game[8])
-            @winner[0] = @old_game[2]
-            return true
-
-        elsif (@old_game[0] == @old_game[1]) && (@old_game[0] == @old_game[2])
-            @winner[0] = @old_game[0]
-            return true
-
-        elsif (@old_game[3] == @old_game[4]) && (@old_game[3] == @old_game[5])
-            @winner[0] = @old_game[3]
-            return true
-
-        elsif (@old_game[6] == @old_game[7]) && (@old_game[6] == @old_game[8])
-            @winner[0] = @old_game[6]
-            return true
-
-        elsif (@old_game[0] == @old_game[4]) && (old_game[0] == @old_game[8])
-            @winner = @old_game[0]
-            return true
-
-        elsif (@old_game[2] == @old_game[4]) && (@old_game[2] == @old_game[6])
-            @winner = @old_game[2]
-            return true
-
-        else
-            return false
-
-        end
-
-    end
-
-    def pausegame()
-        puts @old_game
-        puts "Game paused!"
-        return @game_on = false, @old_game
-
-    end
-
-    def playgame()
-        while @game_on == true
-            if (@player_pick.upcase == "S") | (@player_pick.upcase == "STOP") 
-                if @player_no == true
-                    puts "Game Over! Player #{2} won the game"
-                    break
-
-                elsif @player_no == false
-                    puts "Game Over! Player #{2} won the game"
-                    break 
-
-                else
-                    puts "Game over! Player quiting not specified."
-
-                end
-
-            elsif @old_game.length == 9
-                puts "Game Over. Draw"
-                break #if @old_game.length == 9
-
-            elsif match_three() == true
-                p @winner
-
-            elsif (@player_pick.upcase == "P") | (@player_pick.upcase == "PAUSE")
-                pausegame()
-
-            end
-
-            switch_player()
-
-        end
-
-    end
-
-    def switch_player()
-        @player_no = !@player_no
-
-    end
-
-    def available_moves()
-        @old_game.select.with_index { |avail_move, idx| idx.nil? }
-        return avail_move
-        
-    end
-
-end
-
-#super is useful for instance variables
-#new_game = TickTackToe.new(1,1)
-
-play_game_ = GamePlay.playgame
-
-###new code
 class PlayerInfo
     attr_writer :player_number
     def initialize(player_number)
@@ -218,10 +6,10 @@ class PlayerInfo
     def switch_player()
         @player_number = !@player_number
     end
-    def player_symbol(player_number)
-        if player_number == 1
+    def player_symbol()
+        if @player_number == true
             return symbol = "X"
-        elsif player_number == 2
+        elsif @player_number == false
             return symbol = "O"
         else
             "Player number should be between 1 and 2"
@@ -235,87 +23,98 @@ class GameStates
     def initialize(move_array)
         @move_array = move_array
     end
-    def moves_available()
-        if @move_array.length < 9
-            moves_avail = true
-        elsif (@move_array.include? nil) == true
-            moves_avail = true
-        elsif (@move_array.include? nil) == false
-            moves_avail = false
+    def moves_available() ########################needs a little tunning since used moves can be repeatedly used.
+        count = 0
+        while count <= 8
+            if @move_array[count].class == Fixnum
+                moves_avail = true
+                count = 9
+            else
+                count += 1
+                moves_avail = false
+            end
         end
         return moves_avail
     end
     def match_3_hor()
         count = 0
         match = false
-        until count >= 8 do
-            if @move_array[count] == @move_array[count+1] and @move_array[count] == @move_array[count + 2]
+        until count >= 9 do
+            if (@move_array[count] == @move_array[count+1]) and (@move_array[count] == @move_array[count+2])
                 match = true
-                count = 8
+                match_point = @move_array[count]
+                count = 9
             else
                 count += 3
             end
         end
-        return match, @move_array[count]
+        return match, match_point
     end
     def match_3_vert()
         count = 0
         match = false
-        until count >= 8 do
-            if @move_array[count] == @move_array[count+3] and @move_array[count] == @move_array[count + 6]
+        until count >= 9 do
+            if (@move_array[count] == @move_array[count+3]) and (@move_array[count] == @move_array[count+6])
                 match = true
-                count = 8
+                match_point = @move_array[count]
+                count = 9
             else
-                count += 3
+                count += 1
             end
         end
-        return match, @move_array[count]
+        return match, match_point
     end
     def match_3_diag()
-        count = 0
         match = false
-        if @move_array[count] == @move_array[count+4] and @move_array[count] == @move_array[count+8]
+        if (@move_array[0] == @move_array[4]) and (@move_array[0] == @move_array[8])
             match = true
-        elsif @move_array[count+2] == @move_array[count+4] and @move_array[count+2] == @move_array[count+6]
+            match_point = @move_array[0]
+        elsif (@move_array[2] == @move_array[4]) and (@move_array[2] == @move_array[6])
             match = true
+            match_point = @move_array[2]
         end
-        return match, @move_array[count]
+        return match, match_point
     end
 end
 
 class GameStatus
+    def initialize(move_array)
+        @move_array = move_array
+    end
     def draw()
-        move_array = [1, 2, 4, 8, 5, 6, 7, 8 ,9]
         game_draw = false
         match_3 = false
-        gamestates = GameStates.new(move_array)
+        gamestates = GameStates.new(@move_array)
         moves_available = gamestates.moves_available()
         match_3_diag = gamestates.match_3_diag()
         match_3_hor = gamestates.match_3_hor()
         match_3_vert = gamestates.match_3_vert()
-        if match_3_diag[0] == true or match_3_hor[0] == true or match_3_vert[0] == true
+        if (match_3_diag[0] == true or match_3_hor[0] == true) or (match_3_vert[0] == true)
             match_3 = true
         end
-        if moves_available == false and match_3 == false
+        if moves_available == false && match_3 == false
             game_draw = true
             puts "Game over. The game ends in a draw."
         end
         return game_draw
     end
     def player_win()
-        move_array = ["O", "O", "O", "O", "O", "O", "O", "O" ,"O"]
-        match_3 = false
-        gamestates = GameStates.new(move_array)
+        gamestates = GameStates.new(@move_array)
         match_3_diag_ = gamestates.match_3_diag()
         match_3_hor_ = gamestates.match_3_hor()
         match_3_vert_ = gamestates.match_3_vert()
         if match_3_diag_[0] == true
             win_player_symbol = match_3_diag_[1]
+            win_symbol(win_player_symbol)
         elsif match_3_hor_[0] == true
             win_player_symbol = match_3_hor_[1]
+            win_symbol(win_player_symbol)
         elsif match_3_vert_[0] == true
             win_player_symbol = match_3_vert_[1]
+            win_symbol(win_player_symbol)
         end
+    end
+    def win_symbol(win_player_symbol)
         if win_player_symbol == "O"
             winner = "Player 2"
         elsif win_player_symbol == "X"
@@ -324,3 +123,88 @@ class GameStatus
         return winner
     end
 end
+
+class GamePlay
+    attr_writer :move
+    @@old_move = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    def initialize(move)
+        @move = move
+    end
+    def bad_move()
+        if @move > 9 || @move < 1
+            return true
+        else
+            return false
+        end
+    end
+    def gamestat()
+        gamestatus = GameStatus.new(@@old_move)
+        draw_ = gamestatus.draw()
+        winner = gamestatus.player_win()
+        if draw_ == true or winner == "Player 1" or winner == "Player 2"
+            return true
+        else
+            return false
+        end
+    end
+    def move_pos(play)
+        playerinfo = PlayerInfo.new(play)
+        symbol = playerinfo.player_symbol
+        @@old_move[@move-1] = symbol.chomp
+        return @@old_move
+    end
+end
+
+class Screen < GamePlay
+    def disp_screen()
+        puts "_____________"
+        puts "| #{@@old_move[0]} |" + " #{@@old_move[1]} |" + " #{@@old_move[2]} |"
+        puts "_____________"
+        puts "| #{@@old_move[3]} |" + " #{@@old_move[4]} |" + " #{@@old_move[5]} |"
+        puts "_____________"
+        puts "| #{@@old_move[6]} |" + " #{@@old_move[7]} |" + " #{@@old_move[8]} |"
+        puts "_____________"
+    end
+end
+class GameStart
+    class << self
+        def start_game()
+            gamestat_ = false # Game is still on going
+            play = true #Player number true = player 1 false = player 2.
+            Screen.new("").disp_screen() #The first screen displayed.
+            while  gamestat_ == false
+                playerinfo = PlayerInfo.new(play)
+                puts "Get input move from user"
+                move = gets.chomp.to_i
+                gameplay = GamePlay.new(move)
+                bad_move_ = gameplay.bad_move()
+                move_pos_ = gameplay.move_pos(play)
+                gamestatus = GameStatus.new(move_pos_)
+                draw_ = gamestatus.draw()
+                winner = gamestatus.player_win()
+                if bad_move_ == true
+                    puts "Invalid move made. Please enter valid move between 1 and 9."
+                elsif (bad_move_ == false && draw_ == true)
+                    Screen.new("").disp_screen() #Display screen before declaring the game as a draw
+                    puts "Game ended in a draw!!!"
+                    gamestat_ = true # terminating condition
+                    return 
+                elsif (bad_move_ == false ) && (winner == "Player 1" or winner == "Player 2") 
+                    gamestatus.player_win()
+                    gamestat_ = true
+                    Screen.new("").disp_screen() #Display screen before declaring the game as a win for one of the players
+                    p "Game Over. " + winner + " won the game."
+                    return
+                elsif (bad_move_ == false && gamestat_ == false) #Yet no winner
+                    screen = Screen.new(move)
+                    disp = screen.disp_screen()
+                    play = playerinfo.switch_player()
+                end
+            end
+        end
+    end
+end
+
+#######################  Getting the game under way!!!!!!!!!  ###################
+GameStart.start_game() 
+#######################   THE END!!!!!!! ########################################
